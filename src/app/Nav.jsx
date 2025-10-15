409b6ae3c2566fafd77b23e998f0266c8571862b
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-10-15 17:05:34 +0300

    asd

--- src/app/Nav.jsx
+++ src/app/Nav.jsx
@@ -106,7 +106,7 @@ export default function Navbar() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.8 }}
-
+                style={{ backgroundImage: "url('/bg.png')" }}
         className="w-full  bg-cover bg-center bg-no-repeat flex items-center md:items-start justify-center md:justify-end"
       >
         <motion.div
